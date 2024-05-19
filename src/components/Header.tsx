9058acb5e70f74bdd7b605055c6d9c6ad72e96c8
Skorohod Roman <skorohodroman921@gmail.com>
2024-05-19 12:15:26 +0300

    Version_0.1.0

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,13 @@
+import { NavLink } from 'react-router-dom';
 import styled from 'styled-components';
 
 function Header() {
   return (
     <Container>
       Header
+      <NavLink to="/">Home</NavLink>
+      <NavLink to="/login">Login</NavLink>
+      <NavLink to="/register">Register</NavLink>
     </Container>
   );
 }
