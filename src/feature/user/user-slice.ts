874962b8d3a6aaec91c1c88a87bb5880782d4de0
Skorohod Roman <skorohodroman921@gmail.com>
2024-05-26 13:52:54 +0300

    Version_0.1.3

--- src/feature/user/user-slice.ts
+++ src/feature/user/user-slice.ts
@@ -1,4 +1,5 @@
-import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { createSlice } from "@reduxjs/toolkit";
+// PayloadAction
 import { IUser } from "../../types/user";
 
 const initialState: IUser = {
